2c913a775446b4c9b1c9c0d6cc677062420a8895
ian <ianfarr44@gmail.com>
2022-06-29 17:44:26 -0700

    analytics

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,9 +4,18 @@ import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 
+// Analytics
+import ReactGA from 'react-ga';
+function initializeReactGA() {
+  ReactGA.initialize('G-RR68ESZ081');
+  ReactGA.pageview('/homepage');
+}
+initializeReactGA();
+
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
+      initializeReactGA();
       <App />
     </HashRouter>
   </Provider>
