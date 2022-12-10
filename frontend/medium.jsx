877676b80089166e418264cf15a71e514a5fae2d
ian <ianfarr44@gmail.com>
2022-12-10 14:50:40 -0500

    remove analytics

--- frontend/medium.jsx
+++ frontend/medium.jsx
@@ -4,16 +4,8 @@ import ReactDOM from 'react-dom';
 //Components
 import Root from './components/root';
 import configureStore from './store/store';
-// Analytics
-// import ReactGA from 'react-ga';
-
-// function initializeReactGA() {
-//   ReactGA.initialize('G-RR68ESZ081');
-//   ReactGA.pageview('/homepage');
-// }
 
 document.addEventListener('DOMContentLoaded', () => {
-  initializeReactGA();
   let store;
   if (window.currentUser) {
     const preloadedState = {
