2817a59110462ce02982c74ad8dd5f34aabd1979
ian <ianfarr44@gmail.com>
2023-11-24 10:41:35 -0500

    Revert "remove analytics"
    This reverts commit 877676b80089166e418264cf15a71e514a5fae2d.

--- frontend/medium.jsx
+++ frontend/medium.jsx
@@ -4,8 +4,16 @@ import ReactDOM from 'react-dom';
 //Components
 import Root from './components/root';
 import configureStore from './store/store';
+// Analytics
+// import ReactGA from 'react-ga';
+
+// function initializeReactGA() {
+//   ReactGA.initialize('G-RR68ESZ081');
+//   ReactGA.pageview('/homepage');
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
+  initializeReactGA();
   let store;
   if (window.currentUser) {
     const preloadedState = {
