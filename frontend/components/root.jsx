c231bcd4d2e02c22497f433d01e11ef425cfa3e5
ian <ianfarr44@gmail.com>
2023-11-24 10:41:47 -0500

    Revert "maintenence"
    This reverts commit b36193a25a8206093a34993f93db84ff74af2979.

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,10 +4,18 @@ import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 
+// Analytics
+// import ReactGA from 'react-ga';
+// function initializeReactGA() {
+//   ReactGA.initialize('G-RR68ESZ081');
+//   ReactGA.pageview('/homepage');
+// }
+
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
       <App>
+        {initializeReactGA()}
       </App>
     </HashRouter>
   </Provider>
